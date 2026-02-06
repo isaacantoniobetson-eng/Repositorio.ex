22a758ca7f769bc917f203cf37f18841b80a9084
isaacantoniobetson-eng <isaacantoniobetson@gmail.com>
2026-02-06 09:25:16 +0100

    feature: Livraria digital
    Biblioteca e Repositório digital

---
+++ library/assets/css/efeitos.js
@@ -0,0 +1,18 @@
+// Alterar o texto do elemento quando passar o mouse por cima
+
+
+// Alterar o texto do elemento quando passar o mouse por cima
+
+$('.sms').hover(function(){
+
+		$(this).text('Adicione os seus dados de contacto');
+
+
+
+$('#sms').hover(function(){
+
+		$(this).text('Adicione os seus dados de contacto');
+
+});
+
+}); 
